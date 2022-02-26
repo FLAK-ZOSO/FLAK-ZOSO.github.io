2d0ba5142b2cd0aabc4fcf93c5dcac7b4f2e05ef
FLAK-ZOSO <mattia.marchese.2006@gmail.com>
2022-02-26 22:43:58 +0100

    Improved with classes.js Markdown converter

--- src/game/Markdown/classes.js
+++ src/game/Markdown/classes.js
@@ -1,5 +1,12 @@
-Array.from(document.getElementsByClassName('spoiler')).forEach(element => {
-    element.addEventListener('click', function() {
-        element.classList = ['after-spoiler'];
+function spoiler() {
+    Array.from(document.getElementsByClassName('spoiler')).forEach(element => {
+        element.addEventListener('click', function() {
+            element.classList = ['after-spoiler'];
+        })
     })
-})
+}
+
+
+document.getElementById('convert').addEventListener('click', function() {
+    setTimeout(spoiler, 10);
+});
